4c6698ebb9faea449b36b6ce85eca2315c452a5f
TSG <TSG@TSGAC>
2020-07-06 07:07:00 -0700

    7.6.2020  Added Read from csv, txt, etc. to displays the current database info.

--- Program.cs
+++ Program.cs
@@ -4,15 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Net.Http.Headers;
 
 namespace OMDBmain
 {
     internal class Program
     {
-        private const int V = 0;
-
-        public static string OMDB { get; private set; }
-
+ 
 
         /*The obsucre Music Database!
 A database of strange and wonderful music from around the world.
@@ -29,34 +27,50 @@ V.1.0 */
 
             Console.ForegroundColor = ConsoleColor.Green;
 
-            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor );
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
             Console.ResetColor();
 
 
-                // User inputs Artist and certain details
-                string dataBase = "";
-            Console.WriteLine("OMDB! What would you like to add?  ");
-            Console.Write("Artist: ");
-            string band = Console.ReadLine();
-            Console.Write("Year Formed: ");
-            string formationDate = Console.ReadLine();
-            Console.Write("Favorite Album: ");
-            string favoriteAlbum = Console.ReadLine();
-            Console.Write("Year Of Release: ");
-            string yearOfRelease = Console.ReadLine();
-            Console.Write("Number Of Songs: ");
-            string numberOfSongs = Console.ReadLine();
-            Console.Write("Genre: ");
-            string genre = Console.ReadLine();
-            Console.Write(band + "\n" + formationDate + "\n" + favoriteAlbum + "\n" + yearOfRelease + "\n" + numberOfSongs + "\n" + genre + "\n");
 
-            // Saving additions to file
-            System.Threading.Tasks.Task task = File.AppendAllTextAsync(@"C:\Users\TSG\source\repos\OMDB\Data.txt", dataBase);
-            _ = task;
+            // User inputs Artist and certain details
+            
+            
+                Console.WriteLine("OMDB! What would you like to add?  ");
+                Console.Write("Artist: ");
+                string band = Console.ReadLine();
+                Console.Write("Year Formed: ");
+                string formationDate = Console.ReadLine();
+                Console.Write("Favorite Album: ");
+                string favoriteAlbum = Console.ReadLine();
+                Console.Write("Year Of Release: ");
+                string yearOfRelease = Console.ReadLine();
+                Console.Write("Number Of Songs: ");
+                string numberOfSongs = Console.ReadLine();
+                Console.Write("Genre: ");
+                string genre = Console.ReadLine();
+                Console.Write(band + "\n" + formationDate + "\n" + favoriteAlbum + "\n" + yearOfRelease + "\n" + numberOfSongs + "\n" + genre + "\n");
 
-            string textFilePath = @"C:\Users\TSG\source\repos\OMDB\Data.txt";
 
+            //Saving additions to file
+
+
+            // Reads from csv, txt, etc. and displays the current database info
+            string currenDirectory = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currenDirectory);
+            var fileName = Path.Combine(directory.FullName, "OMDB.csv");
+            var fileContents = ReadFile(fileName);
+            Console.WriteLine(fileContents);
+        
+
+    }
+
+        private static string ReadFile(string fileName)
+        {
+            using (var reader = new StreamReader(fileName))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
