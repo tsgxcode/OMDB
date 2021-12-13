34f0b720724c38a847424145dff4d8532910743d
Scott George <tsgcats@gmail.com>
2021-12-13 06:31:50 -0800

    12.13.2021 Cleaned up some empty lines. Still working the remove fuunction.

--- Progarm.cs
+++ Progarm.cs
@@ -8,7 +8,6 @@ using Newtonsoft.Json;
 using OMDB;
 using System.Text.Json.Serialization;
 
-
 namespace OMDB
 {
     class Program
@@ -20,7 +19,6 @@ namespace OMDB
         public static string formationDate { get; set; }
         public static string artist { get; set; }
         
-
         /*The obsucre Music Database!
           A database of strange and wonderful music from around the world.
           Concept, design and code by T.Scott George - (C) 2020
@@ -34,20 +32,15 @@ namespace OMDB
             string appAuthor = " (C) 2020 T. Scott George";
 
             Console.ForegroundColor = ConsoleColor.Green;
-
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
-
             Console.ResetColor();
-
 
             //Master loop
             do
             {
                 Console.WriteLine("OML! What would you like to add? ");
 
-
                 LogNewArtist();
-                removeArtist();
                 Console.WriteLine("Would you like to see All artist's albums? ");
                 if (Console.ReadLine().ToUpper() == "YES")
                 {
@@ -58,12 +51,8 @@ namespace OMDB
               
             }
             while (Console.ReadLine().ToUpper() == "YES");
-
-
-          
-    
-
         }
+
         //User adds artist's info to the database
         public static void LogNewArtist()
         {
@@ -80,14 +69,10 @@ namespace OMDB
             Console.Write("Genre: ");
             genre = Console.ReadLine();
 
-
-
             //Writing to a .txt file
-            File.AppendAllText(@"C:\Users\TSG\source\repos\OMDB\Data.txt", $"{"Artist: " + artist}. {"Formation Date: " + formationDate}. {"Favorite Album: " + favoriteAlbum}. {"Year Of Release: " + yearOfRelease}. {"Number Of Songs: " + numberOfSongs}. {"Genre: " + genre}" + Environment.NewLine);
+            File.AppendAllText(@"C:\Users\TSGA\source\repos\tsgxcode\OMDB\Data.txt", $"{"Artist: " + artist}. {"Formation Date: " + formationDate}. {"Favorite Album: " + favoriteAlbum}. {"Year Of Release: " + yearOfRelease}. {"Number Of Songs: " + numberOfSongs}. {"Genre: " + genre}" + Environment.NewLine);
         }
-
         public static void OutPutLog()
-       
         {
             // Reads from csv, txt, etc. and displays the current database info
             string currentDirectory = Directory.GetCurrentDirectory();
@@ -95,13 +80,12 @@ namespace OMDB
             var fileName = Path.Combine(directory.FullName, "Data.txt");
             var fileContents = ReadFile(fileName);
             string[] fileLines = fileContents.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            removeArtist();
+            
             foreach (var line in fileLines)
             {
                 Console.WriteLine(line);
             }
         }
-
         public static string ReadFile(string fileName)
         {
             using (var reader = new StreamReader(fileName))
@@ -110,14 +94,12 @@ namespace OMDB
             }
         }
         //Removing an artist from the database 
-        public static void removeArtist()
-         {
-             string name = Console.ReadLine();
-             List<string> lst = File.ReadAllLines("Data.txt").Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
-             lst.RemoveAll(x => x.Split('.')[0].Equals(name));
-             File.WriteAllLines("Data.txt", lst);
-         }
-
-      
+        public static void LogRemoveArtist()
+        {
+            string name = Console.ReadLine();
+            List<string> lst = File.ReadAllLines("Data.txt").Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+            lst.RemoveAll(x => x.Split('.')[0].Equals(name));
+            File.WriteAllLines("Data.txt", lst);
+        }
     }
 } 
